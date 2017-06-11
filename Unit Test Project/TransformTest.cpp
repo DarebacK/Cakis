3cebd4548e2bcb7edc76412eac8701f0779402d3
Pavel Morcinek <PavelMorcinek@seznam.cz>
2017-06-11 21:32:44 +0200

    Unit Test Project setup

---
+++ Unit Test Project/TransformTest.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestProject
+{		
+	TEST_CLASS(TransformTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
